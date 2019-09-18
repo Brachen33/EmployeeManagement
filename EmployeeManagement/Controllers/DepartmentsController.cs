d05665dc02b942143402b6a8d759af181178840f
adam.keith.masters <adam.keith.masters@gmail.com>
2019-09-18 09:38:36 -0700

    First initail commit to dev

---
+++ EmployeeManagement/Controllers/DepartmentsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Controllers
+{
+    public class DepartmentsController
+    {
+        public string List()
+        {
+            return "List() of Departments";
+        }
+
+        public string Details()
+        {
+            return "Details() of Departments";
+        }
+
+    }
+}

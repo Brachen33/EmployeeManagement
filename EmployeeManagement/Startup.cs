7612d002e3d9b737e24c43a5288b49ee5a4eff23
adam.keith.masters <adam.keith.masters@gmail.com>
2019-09-19 19:38:19 -0700

    This is a second test commit to development

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -23,6 +23,9 @@ namespace EmployeeManagement
         }
 
 
+        // Test Commit
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
